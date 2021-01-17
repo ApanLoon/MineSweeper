ce51db0b5095be7d0d6be33f209beb635b14c16b
ApanLoon <apanloon@live.co.uk>
2021-01-17 20:51:39 +0100

    Simple mouse look.

--- MineSweeper/Assets/Scripts/MonoBehaviours/PlayerController.cs
+++ MineSweeper/Assets/Scripts/MonoBehaviours/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 [RequireComponent (typeof (Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    protected float MouseSensitivity = .004f;
     protected float ThrustStrength = 40f;
     protected float BreakStrength = 20f;
     protected float StrafeStrength = 20f;
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour
 
     protected Vector3 Thrust;
     protected Vector3 Strafe;
+    protected Vector3 Rotation;
 
     private void Start()
     {
@@ -20,6 +22,11 @@ public class PlayerController : MonoBehaviour
         Rigidbody = GetComponent<Rigidbody>();
 
         GameEventManager.Instance.OnGameStateChanged += OnGameStateChanged;
+    }
+
+    private void Update()
+    {
+        transform.eulerAngles = Rotation;
     }
 
     private void FixedUpdate()
@@ -37,29 +44,37 @@ public class PlayerController : MonoBehaviour
     protected void OnGameStateChanged (GameState gameState)
     {
         PlayerInput.enabled = gameState == GameState.Playing;
+        Cursor.visible = gameState != GameState.Playing;
     }
 
     public void OnThrust (InputValue input)
     {
         float value = input.Get<float>();
-        Thrust = Vector3.forward * value * (value > 0 ? ThrustStrength : BreakStrength);
+        Thrust = transform.forward * value * (value > 0 ? ThrustStrength : BreakStrength);
     }
 
     public void OnStrafe (InputValue input)
     {
         float value = input.Get<float>();
-        Strafe = Vector3.right * value * StrafeStrength;
+        Strafe = transform.right * value * StrafeStrength;
     }
 
-    public void OnOpenSector(InputValue input)
+    public void OnOpenSector (InputValue input)
     {
         bool value = input.Get<float>() > 0.5f;
         Debug.Log($"OnOpenSector: {value}");
     }
     
-    public void OnShieldSector(InputValue input)
+    public void OnShieldSector (InputValue input)
     {
         bool value = input.Get<float>() > 0.5f;
         Debug.Log($"OnShieldSector: {value}");
+    }
+
+    public void OnAim (InputValue input)
+    {
+        Vector2 value = input.Get<Vector2>() * MouseSensitivity;
+        Rotation.x += value.y;
+        Rotation.y += value.x;
     }
 }
